1405e568a57514ac05873d870a800c6a821f0df0
DanielM0217 <daniel.martinez27@login.cuny.edu>
2025-10-22 21:15:43 -0400

    Completed Project

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const container = document.querySelector('#slideshow_container');
+const slider = document.querySelector('.slider');
+const slides = document.querySelector('.slides');
 
 class Slide {
-    constructor(image, name, score){
+    constructor(image, name, score, synopsis){
             this.image = image;
             this.name = name;
             this.score = score;
+            this.synopsis = synopsis;
         }
     
-        displaySlide() {
+        displaySlide(index) {
+            const slideButton = document.createElement('a');
+            slideButton.href = `#slide-${index + 1}`;
+            slideButton.innerText = `${index + 1}`
+            slider.appendChild(slideButton);
+            
             const slideDiv = document.createElement('div');
-            slideDiv.classList.add('slide');
+            slideDiv.id = `slide-${index + 1}`;
 
             const imageEl = document.createElement('img');
             imageEl.src = this.image;
+            slideDiv.appendChild(imageEl);
 
-            const nameEl = document.createElement('h2');
+            const sectionEl = document.createElement('section');
+
+            const nameEl = document.createElement('h5');
             nameEl.innerText = this.name;
+            sectionEl.appendChild(nameEl);
 
-            const scoreEl = document.createElement('h3');
-            scoreEl.innerText = this.score;
+            const scoreEl = document.createElement('h6');
+            scoreEl.innerText = `Score: ${this.score}`;
+            sectionEl.appendChild(scoreEl);
 
+            const synopsisEl = document.createElement('p');
+            synopsisEl.innerText = this.synopsis;
+            sectionEl.appendChild(synopsisEl);
+
+            slideDiv.appendChild(sectionEl);
+            slides.appendChild(slideDiv);
+            return slideDiv;
+            
         }
     }
 
@@ -27,8 +48,8 @@ class Slide {
 function displaySlides(data) {
 
     for (let i = 0; i < data.data.length; i++){
-        const element = document.createElement('div');
-        let Slide1 = new Slide(data.data[i].images.jpg.large_image_url, data.data[i].title_english, data.data[i].score);
+        let Slide1 = new Slide(data.data[i].images.jpg.large_image_url, data.data[i].title_english || data.data[i].title, data.data[i].score, data.data[i].synopsis);
+        Slide1.displaySlide(i);
         console.log(Slide1)
     }
 }
